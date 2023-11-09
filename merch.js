4f8f6d12477dc71a3c794626dbf10b4d3bfe626b
kacpergilka <150334934+kacpergilka@users.noreply.github.com>
2023-11-09 10:09:09 +0100

    Add files via upload

---
+++ merch.js
@@ -0,0 +1,4 @@
+function toggleMerchOptions() {
+    const merchOptions = document.getElementById('merchOptions');
+    merchOptions.style.display = merchOptions.style.display === 'block' ? 'none' : 'block';
+}
